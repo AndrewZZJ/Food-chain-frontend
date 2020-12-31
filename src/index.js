166f5df8370aaa1d86fed7c0b15f68d5dbcbbdd5
Josh <a23971860@gmail.com>
2020-12-30 20:38:32 -0800

    change component name

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MilestoneCard from './Component/Milestones';
+import MilestoneCard from './component/Milestones';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
