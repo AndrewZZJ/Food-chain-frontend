67bd25c6a1d09677122f0f52cc290a367b459628
LuisHsu <luishsuplus@gmail.com>
2020-12-31 16:55:06 -0800

    React-redux stores

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Font awesome
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-
-import LoginPage from './pages/LoginPage';
-import WaitingPage from './pages/WaitingPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+    BrowserRouter as Router,
+    Switch,
+    Route
 } from "react-router-dom";
+import {Provider} from "react-redux";
+
+import LoginPage from "./pages/LoginPage";
+import WaitingPage from "./pages/WaitingPage";
+
+import "./index.css";
+import store from "./store";
 
 library.add(
-  faInfoCircle
+    faInfoCircle
 );
 
 ReactDOM.render(
-  <Router>
-    <Switch>
-      <Route exact path="/waiting">
-        <WaitingPage />
-      </Route>
-      <Route path="/">
-        <LoginPage />
-      </Route>
-    </Switch>
-  </Router>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path="/waiting">
+                    <WaitingPage />
+                </Route>
+                <Route path="/">
+                    <LoginPage />
+                </Route>
+            </Switch>
+        </Router>
+    </Provider>,
+    document.getElementById("root")
 );
