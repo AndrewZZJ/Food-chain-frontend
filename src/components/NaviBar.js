d28ac74443206f8e620d3c6f9c05ff1eeb1b8307
LuisHsu <luishsuplus@gmail.com>
2021-01-01 20:03:01 -0800

    Bind username to navibar

--- src/components/NaviBar.js
+++ src/components/NaviBar.js
@@ -1,15 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Nav, Navbar, Dropdown, DropdownButton } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./NaviBar.css";
 
-export default function NaviBar(){
+function NaviBar(props){
     return (
         <Navbar id="navbar" expand="lg" variant="light">
             <Navbar.Brand><img src="/logo.svg" /></Navbar.Brand>
             <Nav className="ml-auto">
-                <DropdownButton id="navdrop" title="BBB" variant="link" menuAlign="right">
+                <DropdownButton id="navdrop" title={props.username} variant="link" menuAlign="right">
                     <Dropdown.Item target="_blank" href="/FCM_Rules_EN_v3.pdf">
                         <b>Rules</b>
                         <FontAwesomeIcon className="ml-auto" icon="info-circle" />
@@ -19,3 +20,9 @@ export default function NaviBar(){
         </Navbar>
     );
 }
+
+const mapStateToProps = state => ({
+    username: state.user.username,
+});
+
+export default connect(mapStateToProps, null)(NaviBar);
