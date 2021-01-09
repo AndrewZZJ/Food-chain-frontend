5964530c39ee0ae12ade491ca629313de647b0c9
JordenChang <jordenchang55@gmail.com>
2021-01-08 22:32:37 -0800

    Refactor socket event handle

--- src/components/Socket.js
+++ src/components/Socket.js
@@ -1,27 +1,32 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { connect } from "react-redux";
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
+import { updateSocketEvent } from "../reducers/socket";
 
 const API_HOST = process.env.API_HOST;
 
-function Socket(props){
+const socketEvents = ["launch", "player_list_update"];
+
+function Socket(props) {
     const [socket, createSocket] = useState(null);
+
     useEffect(() => {
-        if(typeof props.token === "string"){
-            let newSocket = io(API_HOST, {auth: {token: props.token}});
-            props.events.forEach(events => Object.keys(events).forEach(key => {
-                newSocket.on(key, (...args) => {
-                    events[key](props.dispatch, ...args);
-                });
-            }));
+        if(typeof props.token === "string") {
+            let newSocket = io(API_HOST, { auth: { token: props.token } });
+
+            socketEvents.forEach(eventName => {
+                newSocket.on(eventName, (message) => props.updateSocketEvent(eventName, message));
+            });
             createSocket(newSocket);
         }
     }, [props.token]);
+
     useEffect(() => {
-        if(socket){
+        if(socket) {
             socket.emit(props.message.event, props.message.data);
         }
     }, [props.message]);
+
     return null;
 }
 
@@ -29,8 +34,8 @@ const mapStateToProps = state => ({
     token: state.user.token,
     message: state.socket.message,
 });
-const mapDispatchToProps = dispatch => ({
-    dispatch,
-});
+const mapDispatchToProps = {
+    updateSocketEvent,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Socket);
