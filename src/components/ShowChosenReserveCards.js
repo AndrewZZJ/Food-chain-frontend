d65d641756ba2d5f2d748bb1aeaad2f36052c9a7
LuisHsu <luishsuplus@gmail.com>
2021-01-09 15:57:27 -0800

    Fix show chosen reserve card

--- src/components/ShowChosenReserveCards.js
+++ src/components/ShowChosenReserveCards.js
@@ -1,36 +1,37 @@
 import React from "react";
-import { Button, Form } from "react-bootstrap";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
+import PropTypes from "prop-types";
+import {Button, Modal, Row, Col} from "react-bootstrap";
+import {ReactSVG} from "react-svg";
+
 import "./ShowChosenReserveCards.css";
 
-function ShowChosenReserveCards() {
-    return (
-        <div className="App">
-            <Container>
-                <Row className="align-items-center">
-                    <Col>
-                        <img src="/Cover.png" alt="Title screen"/>
-                    </Col>
-                    <Col>
-                        <Form>
-                            <Form.Group controlId="formBasicEmail">
-                                <Form.Label style={{ fontFamily: "VTC" }}>USERNAME</Form.Label>
-                                <Form.Control type="email" placeholder="Username"/>
-                                <Form.Text className="text-muted"></Form.Text>
-                            </Form.Group>
-                            <Form.Group controlId="formBasicPassword">
-                                <Form.Label style={{ fontFamily: "VTC" }}>PASSWORD</Form.Label>
-                                <Form.Control type="password" placeholder="Password"/>
-                            </Form.Group>
-                            <Button variant="primary" type="submit" style={{ fontFamily: "VTC" }}>LOGIN</Button>
-                        </Form>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+function ShowChosenReserveCards(props) {
+    return (<Modal 
+        id="show-chosen-reserve"
+        show={props.show}
+        size="xl">
+        <Modal.Header>
+            <Modal.Title>RESERVE CARDS CHOSEN</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Row>
+                {props.cardValues.map(cardValue => (
+                    <Col key={cardValue}><ReactSVG className="reserve-card" src={`/Reserve-${cardValue}.svg`} /></Col>
+                ))}
+            </Row>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="primary" onClick={props.onConfirm}>
+                OK!
+            </Button>
+        </Modal.Footer>
+    </Modal>);
 }
+
+ShowChosenReserveCards.propTypes = {
+    show: PropTypes.bool,
+    onConfirm: PropTypes.func.isRequired,
+    cardValues: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
 
 export default ShowChosenReserveCards;
