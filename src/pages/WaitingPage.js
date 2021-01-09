d8200730225baa279b37b77d2f2398a2015ba659
JordenChang <jordenchang55@gmail.com>
2021-01-09 11:51:52 -0800

    Modify props in WaitingPage

--- src/pages/WaitingPage.js
+++ src/pages/WaitingPage.js
@@ -3,19 +3,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { emit } from "../reducers/socket";
+
 import NaviBar from "../components/NaviBar";
 import "./WaitingPage.css";
 
 function WaitingPage(props) {
-    const setPrepared = (isReady) => props.emit("preparation", { isReady });
-    const exchangePosition = (index) => props.emit("exchange_place", { index });
+    const setPrepared = (prepared) => props.emit("preparation", { prepared });
+    const exchangeIndex = (index) => props.emit("exchange_place", { index });
 
-    const onPreparedClick = () => {
+    function onPreparedClick() {
         if(props.players && props.username) {
             setPrepared(!props.players[props.username].prepared);
         }
-    };
+    }
 
     return (
         <div id="waiting-page">
@@ -23,23 +25,20 @@ function WaitingPage(props) {
             <img id="waiting-room-logo" src="/waiting_room.svg"/>
             <Container id="wrapper">
                 <Row id="player-wrapper">
-                    {Object.keys(props.players).map(username => {
-                        const player = props.players[username];
-                        const { index, prepared } = player;
+                    {[0, 1, 2, 3, 4].map(index => {
+                        const player = Object.entries(props.players).find(entry => (entry[1].index == index));
                         return (
                             <Col key={index} className="player">
                                 <img src={`/restaurant-${index + 1}.svg`} style={{ opacity: player ? 1 : 0.25 }}/>
                                 {player ?
                                     <b className="player-name">{player[0]}</b> :
-                                    <Button
-                                        size="lg" variant="outline-secondary"
-                                        onClick={() => exchangePosition(index)}>
+                                    <Button size="lg" variant="outline-secondary" onClick={() => exchangeIndex(index)}>
                                         <FontAwesomeIcon icon="exchange-alt"/>
                                     </Button>
                                 }
                                 <br/>
                                 {player ? (
-                                    prepared ?
+                                    player[1].prepared ?
                                         <b className="status" style={{ color: "#419641" }}>Ready</b> :
                                         <b className="status" style={{ color: "#CB2E25" }}>Waiting</b>
                                 ) : <b className="status" style={{ color: "#777777" }}>Empty</b>}
@@ -60,19 +59,19 @@ function WaitingPage(props) {
         </div>);
 }
 
-const mapStateToProps = state => ({
-    username: state.user.username,
-    players: state.user.players
-});
-
-const mapDispatchToProps = {
-    emit,
-};
-
 WaitingPage.propTypes = {
     username: PropTypes.string,
     players: PropTypes.object,
     emit: PropTypes.func,
+};
+
+const mapStateToProps = state => ({
+    username: state.user.username,
+    players: state.game.players
+});
+
+const mapDispatchToProps = {
+    emit,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(WaitingPage);
